aa59e96f41604b7788617b43d2fbc8391f475979
duynguyen <duynguyen@axon.com>
2023-03-23 21:50:28 +0700

    [Deploy] build webapp code to static folder

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -1,5 +1,5 @@
 import "antd/dist/antd.min.css";
-import {BrowserRouter as Router, Navigate, Route, Routes,} from "react-router-dom";
+import {HashRouter as Router, Route, Routes} from "react-router-dom";
 import {ROUTES} from "./common/Constant";
 import React from "react";
 import MainLayout from "./layout/MainLayout";
@@ -8,29 +8,26 @@ import AdminPage from "./pages/AdminPage";
 import NotFound from "./pages/ErrorPage/NotFound";
 
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route
-          path={ROUTES.Index.path}
-          element={<Navigate to={ROUTES.Base.path} />}
-        />
-        <Route
-          path={ROUTES.Base.path}
-          element={<HistoryPage/>}
-        />
-        <Route
-          path={ROUTES.Admin.path}
-          element={<AdminPage/>}
-        />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route
+                    path={ROUTES.Admin.path}
+                    element={<AdminPage/>}
+                />
+                <Route
+                    path={ROUTES.Base.path}
+                    exact
+                    element={<HistoryPage/>}
+                />
+                <Route path="*" element={<NotFound/>}/>
+            </Routes>
+        </Router>
+    );
 };
 
 const AppWrapper = () => {
-  return <MainLayout component={<App />} />
+    return <MainLayout component={<App/>}/>
 }
 
 export default AppWrapper;
