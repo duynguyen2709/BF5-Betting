9d1d6a6fac834b4a1ffd38f857da5202e3d97aed
Nguyen Anh Duy <duynguyen@Nguyens-MacBook-Air.local>
2024-11-09 15:25:03 +0700

    Update eslint & prettier config

--- src/main/mobile/vite.config.ts
+++ src/main/mobile/vite.config.ts
@@ -1,18 +1,15 @@
-/// <reference types="vitest" />
-
-import legacy from '@vitejs/plugin-legacy'
+import path from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    legacy()
+    react()
   ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   }
 })
